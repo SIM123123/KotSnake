3b7d93f46cd2127f351b85f7a943cad090d4a0bb
Dia <rdiadama@gmail.com>
2023-09-06 15:29:42 -0400

    Début direction.

--- KotSnake/src/main/kotlin/Main.kt
+++ KotSnake/src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     val jeuSnake = JeuSnake(Grille(5,5))
-    jeuSnake.jouer()
+    jeuSnake.jouer();
 val affichageDirection = AffichageDirection();
     affichageDirection.afficherDirection(Direction.NORD);
     affichageDirection.afficherDirection(Direction.SUD);
